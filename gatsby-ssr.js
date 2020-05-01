d2b39b83b9efcb68dd19bbc5febfc9c3e11641e7
hppRC <hpp.ricecake@gmail.com>
2020-05-01 20:19:22 +0900

    :+1: Update change import order

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,8 @@
+import './src/styles/reset.scss';
+import './src/styles/static-global.scss';
 import 'prismjs/plugins/command-line/prism-command-line.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'katex/dist/katex.min.css';
-import './src/styles/reset.scss';
-import './src/styles/static-global.scss';
 import './src/styles/codeblock.scss';
 import './src/styles/katex.scss';
 
