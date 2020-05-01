683ecc5f38bc309cd3436a5dc637679d58a6868e
hppRC <hpp.ricecake@gmail.com>
2020-05-01 11:45:44 +0900

    :sparkles: add progressive scss support

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,10 @@
 import 'prismjs/plugins/command-line/prism-command-line.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'katex/dist/katex.min.css';
-import 'sanitize.css';
+import './src/styles/reset.scss';
+import './src/styles/static-global.scss';
+import './src/styles/codeblock.scss';
+import './src/styles/katex.scss';
 
 import { WrapRootElement, WrapPageElement } from './src/components';
 
